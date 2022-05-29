e8276113efc57fa525e0e94037d8677ff80dff2b
Kutlay Ozger <kutlayozger@gmail.com>
2022-05-29 12:51:45 +0300

    Input button moved UI folder

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment } from 'react';
+import { useSelector } from 'react-redux';
+
+import TodoList from './pages/TodoList';
+import Login from './pages/Login';
+
 
 function App() {
+  const isAuth = useSelector(state => state.auth.isAuthenticated);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      {!isAuth && <Login />}
+      {isAuth && <TodoList/>}
+    </Fragment>
   );
 }
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,25 @@
-import React from "react";
-import Input from "../components/Input"
+import React, {useRef} from "react";
+import Input from "../components/UI/Input"
+import Button from "../components/UI/Button"
 
 const Login = (props) => {
-    return <React.Fragment>
-        <Input placeholder="Email" />
-        <Input placeholder="Password" />
-    </React.Fragment>
+    const emailInputRef = useRef();
+    const submitHandler = (event) => {
+        event.preventDefault();
+        //TODO validate and send
+    }
+    return     <form  onSubmit={submitHandler}>
+    <Input
+      ref={emailInputRef}
+      label='Email'
+      input={{
+        id: 'email',
+        type: 'text',
+      }}
+    />
+    <Button>Login</Button>
+
+  </form>
 }
 
 export default Login;
